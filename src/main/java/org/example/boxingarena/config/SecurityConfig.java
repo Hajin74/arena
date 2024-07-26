52e7eb78c1c42e84e363244ee9e9216646b1917b
Hajin <qwertygoov@naver.com>
2024-07-26 22:08:14 +0900

    feat: 로그아웃

--- src/main/java/org/example/boxingarena/config/SecurityConfig.java
+++ src/main/java/org/example/boxingarena/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.example.boxingarena.config;
 
 import org.example.boxingarena.auth.CustomLoginFilter;
+import org.example.boxingarena.auth.CustomLogoutFilter;
 import org.example.boxingarena.auth.JwtFilter;
 import org.example.boxingarena.auth.JwtUtil;
 import org.example.boxingarena.repository.RefreshJpaRepository;
@@ -62,7 +63,8 @@ public class SecurityConfig {
         // 필터 등록
         http
                 .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
-                .addFilterAt(new CustomLoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, refreshRepository), UsernamePasswordAuthenticationFilter.class);
+                .addFilterAt(new CustomLoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, refreshRepository), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository), LogoutFilter.class);
 
         return http.build();
     }
--- src/main/java/org/example/boxingarena/ui/AuthController.java
+++ src/main/java/org/example/boxingarena/ui/AuthController.java
@@ -1,11 +1,90 @@
 package org.example.boxingarena.ui;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.boxingarena.auth.JwtUtil;
+import org.example.boxingarena.domain.Refresh;
+import org.example.boxingarena.repository.RefreshJpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
+
+    private final JwtUtil jwtUtil;
+    private final RefreshJpaRepository refreshRepository;
+
+    @PostMapping("/reissue")
+    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
+        String refresh = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("refresh")) {
+                refresh = cookie.getValue();
+                log.info("reissue - {}", refresh);
+            }
+        }
+
+        if (refresh == null) {
+            return new ResponseEntity<>("refresh token null", HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            jwtUtil.isExpired(refresh);
+        } catch (ExpiredJwtException e) {
+            return new ResponseEntity<>("refresh token expired", HttpStatus.BAD_REQUEST);
+        }
+
+        String category = jwtUtil.getCategory(refresh);
+        if (!category.equals("refresh")) {
+            return new ResponseEntity<>("invalid refresh token", HttpStatus.BAD_REQUEST);
+        }
+
+        Boolean isExist = refreshRepository.existsByToken(refresh);
+        if (!isExist) {
+            return new ResponseEntity<>("invalid refresh token", HttpStatus.BAD_REQUEST);
+        }
+
+        String email = jwtUtil.getEmail(refresh);
+
+        String newAccess = jwtUtil.createJwt("access", email,  600000L);
+        String newRefresh = jwtUtil.createJwt("refresh", email, 86400000L);
+
+        refreshRepository.deleteByToken(refresh);
+        addRefresh(email, newRefresh, 86400000L);
+
+        response.setHeader("access", newAccess);
+        response.addCookie(createCookie("refresh", newRefresh));
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private void addRefresh(String email, String token, Long expiredMs) {
+        Date date = new Date(System.currentTimeMillis() + expiredMs);
+        Refresh refresh = Refresh.builder()
+                .email(email)
+                .token(token)
+                .expiration(date.toString())
+                .build();
+
+        refreshRepository.save(refresh);
+    }
+
+    private Cookie createCookie(String key, String value) {
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(24*60*60);
+        cookie.setHttpOnly(true);
+
+        return cookie;
+    }
 }
